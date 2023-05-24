6e84b8cf24da11e5bf60305ee88df4959b4fe43d
Connor Smiley <60119998+ConnorSmiley@users.noreply.github.com>
2023-05-24 20:09:37 +0900

    udemy

---
+++ Arrays/AreThereDuplicates.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+const areThereDuplicates = (...input) => {
+}
+
+
+
+
+
+
+
+
+
+
+console.log(areThereDuplicates('a', 'b', 'c', 'a')
+)
+console.log(areThereDuplicates(1, 2, 2)
+)
+console.log(areThereDuplicates(1, 2, 3)
+)
+
--- Arrays/SameFrequency.js
+++ Arrays/SameFrequency.js
@@ -1,19 +1,50 @@
 
+// const sameFrequency = (input, input2) => {
+//     let map = new Map()
+//     let s = input.toString()
+//     let s2 = input2.toString()
+//     if (s.length !== s2.length) return false
+//
+//     for (let i of s){
+//         map.set(i, map.get(i) + 1 || 1)
+//     }
+//
+//     for (let i of s2){
+//         map.delete(i, map.get(i))
+//     }
+//     return (map.size === 0)
+// }
+
+
+
+
+
+
+
 const sameFrequency = (input, input2) => {
     let map = new Map()
-    let s = input.toString()
-    let s2 = input2.toString()
+
+
+    let s = input.toString().split('')
+    let s2 = input2.toString().split('')
+
     if (s.length !== s2.length) return false
 
-    for (let i of s){
-        map.set(i, map.get(i) + 1 || 1)
+    for (let i = 0; i < s.length; i++){
+        map.set(s[i], map.get(s[i]) + 1 || 1)
     }
 
-    for (let i of s2){
-        map.delete(i, map.get(i))
+    for (let i = 0; i < s2.length; i++) {
+        map.delete(s2[i], map.get(s2[i]))
     }
-    return (map.size === 0)
+
+    if (map.size !== 0) {
+        return false
+    }
+    return true
+
 }
+
 
 
 
