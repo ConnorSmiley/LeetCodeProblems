f38618f890c700607d74f70f398ebc351cf4eca5
Connor Smiley <60119998+ConnorSmiley@users.noreply.github.com>
2024-03-22 18:50:12 +0900

    second round

---
+++ Coderbyte/division.js
@@ -0,0 +1,14 @@
+const num1 = 6
+const num2 = 4
+
+
+function division (a,b) {
+    const temp = 0
+    const limit = Math.min(a,b)
+    console.log(limit)
+    for (i in limit) {
+
+    }
+}
+
+console.log(division(num1, num2))
